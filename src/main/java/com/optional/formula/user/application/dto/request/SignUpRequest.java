bd3718d99de01139bb304eb085a844fd3b5c6556
LimdaeIl <optional94@naver.com>
2025-08-18 12:47:03 +0900

    ✨️#13: 민감정보(비밀번호) 노출 방지: 직렬화/로깅 차단
    record의 기본 toString은 모든 필드를 노출하고, Jackson도 기본 설정에선 필드를 그대로 직렬화합니다. 비밀번호가 로그/응답에 노출될 수 있어 보안 리스크입니다.
    
    아래와 같이 WRITE_ONLY와 toString 마스킹을 추가했습니다.
    
    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
    
    그리고 ToString() 메서드 오버라이딩하여 민감정보(비밀번호)를 **** 으로 수정합니다.

--- src/main/java/com/optional/formula/user/application/dto/request/SignUpRequest.java
+++ src/main/java/com/optional/formula/user/application/dto/request/SignUpRequest.java
@@ -1,9 +1,11 @@
 package com.optional.formula.user.application.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
+import org.jetbrains.annotations.NotNull;
 
 @Builder
 public record SignUpRequest(
@@ -16,10 +18,20 @@ public record SignUpRequest(
                 regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-={}|\\[\\]:\";'<>?,./]).{8,}$",
                 message = "비밀번호는 최소 8자 이상이며, 영문자, 숫자, 특수문자를 포함해야 합니다."
         )
+        @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
         String password,
 
         @NotBlank(message = "닉네임: 닉네임은 필수입니다.")
         String nickname
 ) {
 
+    @NotNull
+    @Override
+    public String toString() {
+        return "SignUpRequest{" +
+                "email='" + email + '\'' +
+                ", password='" + "****" + '\'' +
+                ", nickname='" + nickname + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/optional/formula/user/application/dto/request/UpdatePasswordUserRequest.java
+++ src/main/java/com/optional/formula/user/application/dto/request/UpdatePasswordUserRequest.java
@@ -1,7 +1,9 @@
 package com.optional.formula.user.application.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import org.jetbrains.annotations.NotNull;
 
 public record UpdatePasswordUserRequest(
         @NotBlank(message = "비밀번호: 비밀번호는 필수입니다.")
@@ -9,6 +11,7 @@ public record UpdatePasswordUserRequest(
                 regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-={}|\\[\\]:\";'<>?,./]).{8,}$",
                 message = "비밀번호는 최소 8자 이상이며, 영문자, 숫자, 특수문자를 포함해야 합니다."
         )
+        @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
         String currentPassword,
 
         @NotBlank(message = "비밀번호: 비밀번호는 필수입니다.")
@@ -16,7 +19,16 @@ public record UpdatePasswordUserRequest(
                 regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-={}|\\[\\]:\";'<>?,./]).{8,}$",
                 message = "비밀번호는 최소 8자 이상이며, 영문자, 숫자, 특수문자를 포함해야 합니다."
         )
+        @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
         String newPassword
 ) {
 
+    @NotNull
+    @Override
+    public String toString() {
+        return "UpdatePasswordUserRequest{" +
+                "currentPassword='" + "****" + '\'' +
+                ", newPassword='" + "****" + '\'' +
+                '}';
+    }
 }
