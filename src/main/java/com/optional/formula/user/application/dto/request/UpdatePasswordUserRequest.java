e6e322b9ee9897e9f9d9ae69bbf1f495c76e31d4
LimdaeIl <optional94@naver.com>
2025-08-18 12:50:49 +0900

    ✨️#13: Validation 메시지 수정

--- src/main/java/com/optional/formula/user/application/dto/request/UpdatePasswordUserRequest.java
+++ src/main/java/com/optional/formula/user/application/dto/request/UpdatePasswordUserRequest.java
@@ -9,7 +9,7 @@ public record UpdatePasswordUserRequest(
         @NotBlank(message = "비밀번호: 비밀번호는 필수입니다.")
         @Pattern(
                 regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-={}|\\[\\]:\";'<>?,./]).{8,}$",
-                message = "비밀번호는 최소 8자 이상이며, 영문자, 숫자, 특수문자를 포함해야 합니다."
+                message = "비밀번호: 비밀번호는 최소 8자 이상이며, 영문자, 숫자, 특수문자를 포함해야 합니다."
         )
         @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
         String currentPassword,
@@ -17,7 +17,7 @@ public record UpdatePasswordUserRequest(
         @NotBlank(message = "비밀번호: 비밀번호는 필수입니다.")
         @Pattern(
                 regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-={}|\\[\\]:\";'<>?,./]).{8,}$",
-                message = "비밀번호는 최소 8자 이상이며, 영문자, 숫자, 특수문자를 포함해야 합니다."
+                message = "비밀번호: 비밀번호는 최소 8자 이상이며, 영문자, 숫자, 특수문자를 포함해야 합니다."
         )
         @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
         String newPassword
